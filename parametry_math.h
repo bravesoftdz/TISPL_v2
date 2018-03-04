93201d50f9c3ae5f6cf399891bab5b6b055f241c
Martin Kratochvil <kratochvil.martin@seznam.cz>
2018-03-04 23:13:36 +0100

    parametry_math zrušení struktury PO, pouze globální proměnné, takže R bude volat z PO formu jen např. pm.CT=...., oživení input_CT()

--- parametry_math.h
+++ parametry_math.h
@@ -1,21 +1,22 @@
 //---------------------------------------------------------------------------
-
 #ifndef parametry_mathH
 #define parametry_mathH
+//---------------------------------------------------------------------------
 class TParametry_math
 {
 	public:
-		struct TPO
-		{
-			double CT;//celkový technologický èáas objektu
-			double RD;//rychlost dopravníku
-			double DD;//délka kabiny
-			double K;//kapacita
-			double M;//mezera mezi vozíky
-			double dV;//užitná délka vozíku (tj. strana dle rotace v objektu)
-			short  mV;//rozdíl poèet mezer a vozíkù (vìtšinou o jednu ménì tj. 1)
-		};
-		TPO PO;
+    double TT;//takt linky
+		double CT;//celkový technologický èáas objektu
+		double RD;//rychlost dopravníku
+		double DD;//délka kabiny
+		double K;//kapacita
+		double M;//mezera mezi vozíky
+		double dV;//užitná délka vozíku (tj. strana dle rotace v objektu)
+		short  mV;//rozdíl poèet mezer a vozíkù (vìtšinou o jednu ménì tj. 1)
+		bool CT_locked;//stav zámku
+		bool RD_locked;//stav zámku
+		bool DD_locked;//stav zámku
+
 		void input_CT();//pøepoèet souvisejících hodnot vyplývajících ze zmìny CT
 		void input_RD();//pøepoèet souvisejících  hodnot vyplývajících ze zmìny RD
 		void input_DD();//pøepoèet  souvisejících hodnot vyplývajících ze zmìny DD
