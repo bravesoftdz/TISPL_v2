efb1653a2cc1f7075175eccb4d6b43abd6c5779f
Martin Kratochvil <kratochvil.martin@seznam.cz>
2018-04-30 08:03:00 +0200

    cmy:   bool between(double value,double from, double to, bool left_closed=true, bool right_closed=true);//vrací true či falce zda se daná hodnota nachází čí nenachází v intervalu, interval může být uzavřený (tzn. včetně hodnoty hranice intervalu) nebo otevřený a to i rozdílně pro obě meze, implicitně jsou hranice nastaveny na uzavřený interval z obou stran, tzn. do podmínky se zahrnuje včetně obou hodnot

--- my.cpp
+++ my.cpp
@@ -464,3 +464,14 @@ double Cmy::get_timePERpx(double speed,double A)//A je akcelerace
 		else return F->m2px/F->Zoom/speed*1000/A;//vrátí èas na posun o jeden pixel
 }
 /////////////////////////////////////////////////////////////////////////////
+//vrací true èi falce zda se daná hodnota nachází èí nenachází v intervalu, interval mùže být uzavøený (tzn. vèetnì hodnoty hranice intervalu) nebo otevøený a to i rozdílnì pro obì meze, implicitnì jsou hranice nastaveny na uzavøený interval z obou stran, tzn. do podmínky se zahrnuje vèetnì obou hodnot
+bool Cmy::between(double value,double from, double to, bool left_closed, bool right_closed)
+{
+	bool RET=false;
+	if(left_closed && right_closed && from<=to && value>=from && value<=to)RET=true;
+	if(!left_closed && right_closed && from<to && value>from && value<=to)RET=true;
+	if(left_closed && !right_closed && from<to && value>=from && value<to)RET=true;
+	if(!left_closed && !right_closed && from<to && value>from && value<to)RET=true;
+	return RET;
+}
+/////////////////////////////////////////////////////////////////////////////
