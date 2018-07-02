bfee3c2a329fe0fdc5ed6545f4fbf243a16427e6
Martin Kratochvil <kratochvil.martin@seznam.cz>
2018-07-02 11:00:23 +0200

    update gapoTT ColCount->RowCount

--- gapoTT.cpp
+++ gapoTT.cpp
@@ -39,7 +39,7 @@ void __fastcall TF_gapoTT::FormShow(TObject *Sender)
 	unsigned long RowCount=1;//dynamický poèet øádkù, default 1 je pro 0-tý indexový øádek
 	RowCount+=F->d.v.vrat_pocet_objektu_bezNEBOs_prirazenymi_pohonu(false)+F->d.v.vrat_pocet_nepouzivanych_pohonu()+F->d.v.vrat_pocet_objektu_bezNEBOs_prirazenymi_pohonu(true);//PØIDAT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	mGrid->Create(ColCount,RowCount);//samotné vytvoøení matice-tabulky
-	objekty=new Cvektory::TObjekt[ColCount];//dynamické pole, uchovávající ukazatele na objekty v tabulce sloupci objekty
+	objekty=new Cvektory::TObjekt[RowCount];//dynamické pole, uchovávající ukazatele na objekty v tabulce sloupci objekty
 
 	////////plnìní daty - hlavièka////////
 	mGrid->Cells[0][0].Text="pohon";
@@ -149,55 +149,54 @@ void __fastcall TF_gapoTT::FormShow(TObject *Sender)
 	////////pøiøadí celé oblasti bunìk totožné vlastnosti jako u referenèní buòky////////
 	mGrid->SetCells(mGrid->Cells[0][0],15,0,ColCount-1,0);//pro první øádek
 	mGrid->SetCells(mGrid->Cells[0][0],1,0,3,0);//pro první øádek
-//	//v tomto pøípadì šedý sloupec (stejný jako orámování) jako nemìné hodnoty - nedoøešený problém s posunem v AA
+	//v tomto pøípadì šedý sloupec (stejný jako orámování) jako nemìné hodnoty - nedoøešený problém s posunem v AA
 //	mGrid->Cells[ColCount-1][1].Background->Color=(TColor)RGB(240,240,240);
 //	mGrid->SetCells(mGrid->Cells[ColCount-1][1],ColCount-1,2,ColCount-1,RowCount-1);
-//
-//	//manualfit výšky 0-tého øádku (zatím není pøipravena metoda)
+
+	//manualfit výšky 0-tého øádku (zatím není pøipravena metoda)
 	Canvas->Font=mGrid->Cells[16][0].Font;	//nejdelší použitý text
 	mGrid->Rows[0].Height=Canvas->TextWidth(mGrid->Cells[16][0].Text)+mGrid->Cells[16][0].BottomMargin+mGrid->Cells[16][0].BottomBorder->Width/2+mGrid->Cells[16][0].TopMargin+mGrid->Cells[16][0].TopBorder->Width/2;
-//	//manualfit šíøky sloupcù mimo prvního (ten je øešen automaticky níže pomocí SetColumnAutoFit(0);)
+	//manualfit šíøky sloupcù mimo prvního (ten je øešen automaticky níže pomocí SetColumnAutoFit(0);)
 	mGrid->Columns[1].Width=100;mGrid->Columns[3].Width=mGrid->Columns[4].Width=mGrid->Columns[5].Width=mGrid->Columns[6].Width=mGrid->Columns[7].Width=mGrid->Columns[8].Width=mGrid->Columns[9].Width=mGrid->Columns[10].Width=mGrid->Columns[11].Width=mGrid->Columns[12].Width=mGrid->Columns[13].Width=mGrid->Columns[14].Width=35;//ostatní následující sloupce zatím default šíøka
 	mGrid->Columns[2].Width=100;
-	//
-//	//nastavení velikosti nultého sloupce dle obsahu, mùže být umístìno kdekoliv pøed Show(), ale lépe pøed merge metodami
+
+	//nastavení velikosti nultého sloupce dle obsahu, mùže být umístìno kdekoliv pøed Show(), ale lépe pøed merge metodami
 	mGrid->SetColumnAutoFit(0);
 
-
- ////////jednolivé øádky////////
+	////////jednolivé øádky////////
 	unsigned long j=1;//èíslo aktuálnì zpracovávaného øádku, musí zaèínat 1, 0 - je hlavièka
 	////prùchod všemi objekty bez pøiøazených pohonu
 	Cvektory::TObjekt *On=F->d.v.vrat_objekty_bez_pohonu();
 	unsigned long On_pocet=F->d.v.vrat_pocet_objektu_bezNEBOs_prirazenymi_pohonu(false);
 	for(unsigned long i=1;i<On_pocet;i++)//0-nultou buòku nevyužíváme necháváme prázdnou (z dùvodu totožné indexace)
 	{
-			//pole, uchovávající ukazatele na objekty v tabulce sloupci objekty, za úèelem dalšího použití, pouze duplikát objektù, proto se nepropíše do spojáku OBJEKTY
-			objekty[j]=On[i];
-			//pohony
-			mGrid->Cells[0][j].Text="nepøiøazen";
-			//objekty
-			mGrid->Cells[1][j].Text=On[i].short_name;//On[i].short_name;
-			//volby - checkboxy  - Rosta dodelá
-				mGrid->Cells[3][j].Type=mGrid->CHECK;mGrid->Cells[5][j].Type=mGrid->CHECK;
-				mGrid->MergeCells(3,j,4,j);mGrid->MergeCells(5,j,6,j);//slouèení sloupcù
+		//pole, uchovávající ukazatele na objekty v tabulce sloupci objekty, za úèelem dalšího použití, pouze duplikát objektù, proto se nepropíše do spojáku OBJEKTY
+		objekty[j]=On[i];
+		//pohony
+		mGrid->Cells[0][j].Text="nepøiøazen";
+		//objekty
+		mGrid->Cells[1][j].Text=On[i].short_name;//On[i].short_name;
+		//volby - checkboxy  - Rosta dodelá
+		mGrid->Cells[3][j].Type=mGrid->CHECK;mGrid->Cells[5][j].Type=mGrid->CHECK;
+		mGrid->MergeCells(3,j,4,j);mGrid->MergeCells(5,j,6,j);//slouèení sloupcù
 
-				mGrid->Cells[7][j].Type=mGrid->CHECK;mGrid->Cells[9][j].Type=mGrid->CHECK;
-				mGrid->MergeCells(7,j,8,j);mGrid->MergeCells(9,j,10,j);//slouèení sloupcù
+		mGrid->Cells[7][j].Type=mGrid->CHECK;mGrid->Cells[9][j].Type=mGrid->CHECK;
+		mGrid->MergeCells(7,j,8,j);mGrid->MergeCells(9,j,10,j);//slouèení sloupcù
 
-				mGrid->Cells[11][j].Type=mGrid->CHECK;mGrid->Cells[13][j].Type=mGrid->CHECK;
-				mGrid->MergeCells(11,j,12,j);mGrid->MergeCells(13,j,14,j);//slouèení sloupcù
+		mGrid->Cells[11][j].Type=mGrid->CHECK;mGrid->Cells[13][j].Type=mGrid->CHECK;
+		mGrid->MergeCells(11,j,12,j);mGrid->MergeCells(13,j,14,j);//slouèení sloupcù
 
-			//parametry objektù
-			mGrid->Cells[15][j].Text=On[i].CT;
-			mGrid->Cells[16][j].Text=On[i].RD;
-			mGrid->Cells[17][j].Text=On[i].delka_dopravniku;
-			mGrid->Cells[18][j].Text=On[i].kapacita;
-			mGrid->Cells[19][j].Text=On[i].pozice;
-			mGrid->Cells[20][j].Text=On[i].mezera_jig;
-			mGrid->Cells[21][j].Text=On[i].mezera_podvozek;
-			mGrid->Cells[22][j].Text=On[i].rotace;
-			//posun na další øádek výsledné tabulky
-			j++;
+		//parametry objektù
+		mGrid->Cells[15][j].Text=On[i].CT;
+		mGrid->Cells[16][j].Text=On[i].RD;
+		mGrid->Cells[17][j].Text=On[i].delka_dopravniku;
+		mGrid->Cells[18][j].Text=On[i].kapacita;
+		mGrid->Cells[19][j].Text=On[i].pozice;
+		mGrid->Cells[20][j].Text=On[i].mezera_jig;
+		mGrid->Cells[21][j].Text=On[i].mezera_podvozek;
+		mGrid->Cells[22][j].Text=On[i].rotace;
+		//posun na další øádek výsledné tabulky
+		j++;
 	}
 	On=NULL;delete On;
 	if(On_pocet>0)mGrid->MergeCells(0,1,0,On_pocet);//slouèení bunìk objekty bez pohonu
@@ -214,17 +213,15 @@ void __fastcall TF_gapoTT::FormShow(TObject *Sender)
 			mGrid->Cells[0][j].Text=F->d.v.vrat_pohon(i)->name;
 			//objekty
 			mGrid->Cells[1][j].Text="nepøiøazen";
-			//volby - checkboxy - Rostì dodìlá
+			//volby - checkboxy
+			mGrid->Cells[3][j].Type=mGrid->CHECK;mGrid->Cells[5][j].Type=mGrid->CHECK;
+			mGrid->MergeCells(3,j,4,j);mGrid->MergeCells(5,j,6,j);//slouèení sloupcù
 
-				mGrid->Cells[3][j].Type=mGrid->CHECK;mGrid->Cells[5][j].Type=mGrid->CHECK;
-				mGrid->MergeCells(3,j,4,j);mGrid->MergeCells(5,j,6,j);//slouèení sloupcù
+			mGrid->Cells[7][j].Type=mGrid->CHECK;mGrid->Cells[9][j].Type=mGrid->CHECK;
+			mGrid->MergeCells(7,j,8,j);mGrid->MergeCells(9,j,10,j);//slouèení sloupcù
 
-				mGrid->Cells[7][j].Type=mGrid->CHECK;mGrid->Cells[9][j].Type=mGrid->CHECK;
-				mGrid->MergeCells(7,j,8,j);mGrid->MergeCells(9,j,10,j);//slouèení sloupcù
-
-				mGrid->Cells[11][j].Type=mGrid->CHECK;mGrid->Cells[13][j].Type=mGrid->CHECK;
-				mGrid->MergeCells(11,j,12,j);mGrid->MergeCells(13,j,14,j);//slouèení sloupcù
-
+			mGrid->Cells[11][j].Type=mGrid->CHECK;mGrid->Cells[13][j].Type=mGrid->CHECK;
+			mGrid->MergeCells(11,j,12,j);mGrid->MergeCells(13,j,14,j);//slouèení sloupcù
 			//posun na další øádek výsledné tabulky
 			j++;
 		}
@@ -236,16 +233,16 @@ void __fastcall TF_gapoTT::FormShow(TObject *Sender)
 			unsigned long z=0;
 			for(;z<O_pocet;z++)
 			{
-				 //pole, uchovávající ukazatele na objekty v tabulce sloupci objekty, za úèelem dalšího použití, pouze duplikát objektù, proto se nepropíše do spojáku OBJEKTY
-				 objekty[j]=O[z];
-				 //pohony
-				 mGrid->Cells[0][j].Text=O[z].pohon->name;
-				 //objekty
-				 mGrid->Cells[1][j].Text=O[z].short_name;
-				 if(O[z].rezim==0) rezim="S&G";
-				 if(O[z].rezim==1) rezim="Kontinuální";
-				 if(O[z].rezim==2) rezim="Postprocesní";
-				 mGrid->Cells[2][j].Text=rezim;
+				//pole, uchovávající ukazatele na objekty v tabulce sloupci objekty, za úèelem dalšího použití, pouze duplikát objektù, proto se nepropíše do spojáku OBJEKTY
+				objekty[j]=O[z];
+				//pohony
+				mGrid->Cells[0][j].Text=O[z].pohon->name;
+				//objekty
+				mGrid->Cells[1][j].Text=O[z].short_name;
+				if(O[z].rezim==0) rezim="S&G";
+				if(O[z].rezim==1) rezim="Kontinuální";
+				if(O[z].rezim==2) rezim="Postprocesní";
+				mGrid->Cells[2][j].Text=rezim;
 				 //volby - checkboxy
 				mGrid->Cells[3][j].Type=mGrid->CHECK;mGrid->Cells[5][j].Type=mGrid->CHECK;
 				mGrid->MergeCells(3,j,4,j);mGrid->MergeCells(5,j,6,j);//slouèení sloupcù
@@ -258,18 +255,18 @@ void __fastcall TF_gapoTT::FormShow(TObject *Sender)
 				mGrid->MergeCells(11,j,12,j);mGrid->MergeCells(13,j,14,j);//slouèení sloupcù
 			//	}
 
-				 //parametry objektù
-				 mGrid->Cells[15][j].Text=O[z].CT;
-				 mGrid->Cells[16][j].Text=O[z].RD*60.0;
-				 mGrid->Cells[17][j].Text=O[z].delka_dopravniku;
-				 mGrid->Cells[18][j].Text=O[z].kapacita;
-				 mGrid->Cells[19][j].Text=O[z].pozice;
-				 mGrid->Cells[20][j].Text=O[z].mezera_jig;
-				 if(O[z].mezera_podvozek<0.00000004) mGrid->Cells[21][j].Text="0";
-			  	else mGrid->Cells[21][j].Text=O[z].mezera_podvozek;
-				 mGrid->Cells[22][j].Text=O[z].rotace;
-				 //posun na další øádek výsledné tabulky
-				 j++;
+				//parametry objektù
+				mGrid->Cells[15][j].Text=O[z].CT;
+				mGrid->Cells[16][j].Text=O[z].RD*60.0;
+				mGrid->Cells[17][j].Text=O[z].delka_dopravniku;
+				mGrid->Cells[18][j].Text=O[z].kapacita;
+				mGrid->Cells[19][j].Text=O[z].pozice;
+				mGrid->Cells[20][j].Text=O[z].mezera_jig;
+				if(O[z].mezera_podvozek<0.00000004) mGrid->Cells[21][j].Text="0";
+				else mGrid->Cells[21][j].Text=O[z].mezera_podvozek;
+				mGrid->Cells[22][j].Text=O[z].rotace;
+				//posun na další øádek výsledné tabulky
+				j++;
       }                    //Rosa ovìøit, že je OK
 			mGrid->MergeCells(0,j-z,0,j-z+O_pocet-1);//slouèení bunìk pohony
 			O=NULL;delete O;
