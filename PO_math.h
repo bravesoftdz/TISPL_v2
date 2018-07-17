826a3caa5295ebc803961823ea28d5c0c2da4d1b
Martin Kratochvil <kratochvil.martin@seznam.cz>
2018-07-17 11:17:26 +0200

    gapoTT - výpočetní část, pozor, nepřeložitelné, kvůli poznámece "rosta opraví číslování sloupců" - až bude TT form hotov a budou zobrazeny i sloupce R

--- PO_math.h
+++ PO_math.h
@@ -37,15 +37,14 @@ class TPO_math
 		void input_M(); //pøepoèet souvisejících hodnot vyplývajících ze zmìny kritické vozíkové mezery
 		void input_MJ();//pøepoèet souvisejících hodnot vyplývajících ze zmìny mezery jigu
 		void input_MP();//pøepoèet souvisejících hodnot vyplývajících ze zmìny mezery podvozku
+		double Pozice();//vrátí poèet pozic z kapacity, øeší i situaci, kdy je M (mezera) nulová, tj. K==P
 		double K2P(double K);//vrátí poèet pozic z kapacity, øeší i situaci, kdy je M (mezera) nulová, tj. K==P
 		double P2K(double P);//vrátí kapacitu z poètu pozic, øeší i situaci, kdy je M (mezera) nulová, tj. K==P
 		double Mezera();//vrátí velikost mezery dle aktuální rychlosti RD, nehledí na rozteè, ale rovnou poèítá Rx,Rz-testování (to se nepoužívá, protože se používá nacti_rx pøímo v PO)
+		double UDV();//vrátí užitnou délku vozíku dle hodnoty rotace
 	private:
 		Cmy m;
 		double P2K();//vrátí kapacitu z poètu pozic, øeší i situaci, kdy je M (mezera) nulová, tj. K==P
-		double Pozice();//vrátí poèet pozic z kapacity, øeší i situaci, kdy je M (mezera) nulová, tj. K==P
-		double UDV();//vrátí užitnou délku vozíku dle hodnoty rotace
-
 };
 //---------------------------------------------------------------------------
 #endif
