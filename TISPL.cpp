f56a64c405d3a3163a5bc9592d9f7481b6e340a8
Martin Vlcek <m.vlcek@eltep.cz>
2019-03-08 14:28:24 +0100

    Odebrání Unit3, Unit4 a přidání UnitX

--- TISPL.cpp
+++ TISPL.cpp
@@ -37,31 +37,32 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->Initialize();
 		Application->MainFormOnTaskBar = true;
 		Application->CreateForm(__classid(TForm1), &Form1);
-     Application->CreateForm(__classid(TmyMessageBox), &myMessageBox);
-     Application->CreateForm(__classid(TForm_uvod), &Form_uvod);
-     Application->CreateForm(__classid(TForm_parametry), &Form_parametry);
-     Application->CreateForm(__classid(TForm_definice_zakazek), &Form_definice_zakazek);
-     Application->CreateForm(__classid(TForm_jig), &Form_jig);
-     Application->CreateForm(__classid(TForm_cesty), &Form_cesty);
-     Application->CreateForm(__classid(TForm_parametry_linky), &Form_parametry_linky);
-     Application->CreateForm(__classid(TPopUPmenu), &PopUPmenu);
-     Application->CreateForm(__classid(TForm_eDesigner), &Form_eDesigner);
-     Application->CreateForm(__classid(TForm_kalendar), &Form_kalendar);
-     Application->CreateForm(__classid(TForm_osa_info), &Form_osa_info);
-     Application->CreateForm(__classid(TForm_report), &Form_report);
-     Application->CreateForm(__classid(TForm_PL_priority), &Form_PL_priority);
-     Application->CreateForm(__classid(TForm_poznamky), &Form_poznamky);
-     Application->CreateForm(__classid(TForm_objekt_nahled), &Form_objekt_nahled);
-     Application->CreateForm(__classid(TForm_TT_kalkulator), &Form_TT_kalkulator);
-     Application->CreateForm(__classid(TForm_parametry_vozik), &Form_parametry_vozik);
-     Application->CreateForm(__classid(TForm2), &Form2);
-     Application->CreateForm(__classid(TF_gapoR), &F_gapoR);
-     Application->CreateForm(__classid(TF_gapoTT), &F_gapoTT);
-     Application->CreateForm(__classid(TF_gapoV), &F_gapoV);
-     Application->CreateForm(__classid(TForm_adjustace), &Form_adjustace);
-     Application->CreateForm(__classid(TForm_Z_rozliseni), &Form_Z_rozliseni);
-     Application->CreateForm(__classid(TFormX), &FormX);
-     Application->ShowMainForm=false;//zabránìní zobrazení hlavního formu
+		Application->CreateForm(__classid(TmyMessageBox), &myMessageBox);
+		Application->CreateForm(__classid(TForm_uvod), &Form_uvod);
+		Application->CreateForm(__classid(TForm_parametry), &Form_parametry);
+		Application->CreateForm(__classid(TForm_definice_zakazek), &Form_definice_zakazek);
+		Application->CreateForm(__classid(TForm_jig), &Form_jig);
+		Application->CreateForm(__classid(TForm_cesty), &Form_cesty);
+		Application->CreateForm(__classid(TForm_parametry_linky), &Form_parametry_linky);
+		Application->CreateForm(__classid(TPopUPmenu), &PopUPmenu);
+		Application->CreateForm(__classid(TForm_eDesigner), &Form_eDesigner);
+		Application->CreateForm(__classid(TForm_kalendar), &Form_kalendar);
+		Application->CreateForm(__classid(TForm_osa_info), &Form_osa_info);
+		Application->CreateForm(__classid(TForm_report), &Form_report);
+		Application->CreateForm(__classid(TForm_PL_priority), &Form_PL_priority);
+		Application->CreateForm(__classid(TForm_poznamky), &Form_poznamky);
+		Application->CreateForm(__classid(TForm_objekt_nahled), &Form_objekt_nahled);
+		Application->CreateForm(__classid(TForm_TT_kalkulator), &Form_TT_kalkulator);
+		Application->CreateForm(__classid(TForm_parametry_vozik), &Form_parametry_vozik);
+		Application->CreateForm(__classid(TForm2), &Form2);
+		Application->CreateForm(__classid(TF_gapoR), &F_gapoR);
+		Application->CreateForm(__classid(TF_gapoTT), &F_gapoTT);
+		Application->CreateForm(__classid(TF_gapoV), &F_gapoV);
+		Application->CreateForm(__classid(TForm_adjustace), &Form_adjustace);
+		Application->CreateForm(__classid(TForm_Z_rozliseni), &Form_Z_rozliseni);
+		Application->CreateForm(__classid(TFormX), &FormX);
+		Application->CreateForm(__classid(TFormX), &FormX);
+		Application->ShowMainForm=false;//zabránìní zobrazení hlavního formu
 		Application->Run();
 	}
 	catch (Exception &exception)
